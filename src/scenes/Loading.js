0bcc2847c470d4a644967f10d40e1a75a1863a1f
Nick <nicholas.balestrieri@gmail.com>
2020-02-01 21:50:23 -0500

    Remove extra splash screen

--- src/scenes/Loading.js
+++ src/scenes/Loading.js
@@ -8,6 +8,6 @@ export default class Loading extends Phaser.Scene{
     create(){
         this.add.text(50,50, "...Loading")
 
-        setTimeout(() => {this.scene.start("menuScreen")}, 1500)
+        setTimeout(() => {this.scene.start("mainWorld")}, 1500)
     }
 }
--- src/scenes/index.js
+++ src/scenes/index.js
@@ -1,8 +1,7 @@
 import Loading from "./Loading"
-import Menu from "./Menu"
 import World from "./World" 
 import HUD from "./HUD"
 
 // Import scene files and add them below
 
-export default [Loading, Menu, World, HUD]
+export default [Loading, World, HUD]
