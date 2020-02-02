ac6dde6d0c4086f30802d16d015bc65b22c8e0cd
Nick <nicholas.balestrieri@gmail.com>
2020-02-02 11:41:27 -0500

    Add working hammer animation

--- src/scenes/World.js
+++ src/scenes/World.js
@@ -13,7 +13,10 @@ class World extends Phaser.Scene{
 
     preload(){
         this.load.image('boiler', 'src/assets/boiler.png')
-        this.load.spritesheet('robot', 'src/assets/ggj_chamalet_2020.png', {frameWidth:560, frameHeight:760})
+        this.load.spritesheet('robot', 'src/assets/ggj_chamalet_2020.png', {
+            frameWidth:657, 
+            frameHeight:830
+        })
         this.load.image('platform', 'src/assets/floor.png');
         this.load.image('hud', 'src/assets/frame_fin.png');
         this.load.image('box', 'src/assets/box.png');
@@ -28,7 +31,7 @@ class World extends Phaser.Scene{
         this.platform = this.physics.add.staticGroup()
         this.platform.create(400, 600, 'platform').setScale(0.5).refreshBody()
         
-        this.robot = this.physics.add.sprite(400, 100, 'robot')
+        this.robot = this.physics.add.sprite(400, 400, 'robot')
         this.robot.setScale(0.15);
         this.robot.setBounce(0.5, 0.5);
         this.robot.setCollideWorldBounds(true);
@@ -36,28 +39,28 @@ class World extends Phaser.Scene{
         this.boxes = this.physics.add.group({
             key: 'box',
             repeat: 5,
-            setXY: {x:12, y:0, stepX:70}
+            setXY: {x:12, y:300, stepX:70}
         })
         
         this.boxes.children.iterate(function (child) {
             
-            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            child.setBounceY(Phaser.Math.FloatBetween(0.3, 0.3));
             child.setScale(0.2)
             
         });
         
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('robot', { start: 0, end: 5 }),
+            frames: this.anims.generateFrameNumbers('robot', { start: 1, end: 5 }),
             frameRate: 7,
             repeat: -1
         });
         
-        // this.anims.create({
-        //     key: 'turn',
-        //     frames: [ { key: 'robot', frame: 4 } ],
-        //     frameRate: 20
-        // });
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'robot', frame: 0 } ],
+            frameRate: 20
+        });
         
         this.anims.create({
             key: 'right',
@@ -65,6 +68,13 @@ class World extends Phaser.Scene{
             frameRate: 7,
             repeat: -1
         });
+
+        this.anims.create({
+            key: 'hammer',
+            frames: this.anims.generateFrameNumbers('robot', { start:6, end:7 }),
+            frameRate:7,
+            repeat: -1
+        })
         
         this.keys = {
             left : this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
@@ -79,7 +89,8 @@ class World extends Phaser.Scene{
         this.physics.add.collider(this.robot, this.hud)
         
         const touchBox = (robot, box) => {
-            box.disableBody(true, true)
+            // box.disableBody(true, true)
+
         }
         
         this.physics.add.overlap(this.robot, this.boxes, touchBox, null, this)
@@ -99,14 +110,19 @@ class World extends Phaser.Scene{
 
             this.robot.anims.play('right', true);
         }
+        else if(this.keys.down.isDown && (this.robot.body.touching.down || this.robot.body.onFloor())){
+            this.robot.setVelocity(0)
+            this.robot.anims.play('hammer', true);
+        }
         else{
             this.robot.setVelocityX(0);
 
-            // this.robot.anims.play('turn');
+            this.robot.anims.play('turn');
         }
 
         if (this.keys.jump.isDown && (this.robot.body.touching.down || this.robot.body.onFloor())){
             this.robot.setVelocityY(-400);
         }
+
     }
 }
